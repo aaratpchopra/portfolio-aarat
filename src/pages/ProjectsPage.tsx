ce7c80300970c1ec779ab565b33b996ee79990f3
Aarat Prasad Chopra <aarat.chopra@gmail.com>
2022-01-09 19:19:25 +0530

    Minor Edits

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import sanityClient from "../client";
 import { PRPHeading, PRPSubHeading } from "../Constants";
 
